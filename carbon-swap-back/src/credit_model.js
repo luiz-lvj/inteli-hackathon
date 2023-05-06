23e19b91120915f637d804b406b0199e77b7ff74
Daniel Yuki <danielyukihiga1@gmail.com>
2023-05-06 19:41:21 -0300

    back-end bizonho

---
+++ carbon-swap-back/src/credit_model.js
@@ -0,0 +1,72 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "my_credit",
+  host: "localhost",
+  database: "cswapdb",
+  password: "olaf",
+  port: 1234,
+});
+
+const getcredits = () => {
+  return new Promise(function (resolve, reject) {
+    pool.query("SELECT * FROM creditsV2 ORDER BY id ASC", (error, results) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(results.rows);
+    });
+  });
+};
+
+const createcredit = (body) => {
+  return new Promise(function (resolve, reject) {
+    const { creditAddress,owner,price, quantity, deadline,v,r,s  } = body;
+    pool.query(
+      // `INSERT INTO creditsV2 ("creditAddress",owner,price, quantity, deadline,v,r,s ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
+      "INSERT INTO creditsV2 (creditAddress,owner,price, quantity, deadline,v,r,s ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+      [creditAddress,owner,price, quantity, deadline,v,r,s ],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`A new credit has been added: ${results}`);
+      }
+    );
+  });
+};
+
+const deletecredit = (id) => {
+  return new Promise(function (resolve, reject) {
+    //const id = parseInt(request.params.id);
+    pool.query("DELETE FROM creditsV2 WHERE id = $1", [id], (error, results) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(`credit deleted with ID: ${id}`);
+    });
+  });
+};
+
+const getcredit = (adrs) => {
+  return new Promise(function (resolve, reject) {
+    //pool.query(`SELECT * FROM creditsV2 WHERE "creditAddress" = $1`, [adrs], (error, results) => {
+    pool.query("SELECT * FROM creditsV2 WHERE creditAddress = $1", [adrs], (error, results) => {
+      if (error) {
+        reject(error);
+      }
+      if (results.rows.length === 0) {
+        resolve(`No credit found with Address: ${adrs}`);
+      } else {
+        resolve(results.rows);
+      }
+    });
+  });
+};
+
+module.exports = {
+  getcredits,
+  getcredit,
+  createcredit,
+  deletecredit,
+};
---
+++ carbon-swap-back/src/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+const credit_model = require("./credit_model");
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  credit_model
+    .getcredits()
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/get-sell-order/:creditAddress", (req, res) => {
+  credit_model
+    .getcredit(req.params.creditAddress)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post("/new-sell-order", (req, res) => {
+  credit_model
+    .createcredit(req.body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/credits/:id", (req, res) => {
+  credit_model
+    .deletecredit(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
