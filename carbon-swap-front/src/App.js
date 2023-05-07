04e075a4114009ddbd9f295a1915c298d5b83e26
Daniel Yuki <danielyukihiga1@gmail.com>
2023-05-07 04:29:07 -0300

    footer + new home

--- carbon-swap-front/src/App.js
+++ carbon-swap-front/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Send from './components/Send';
 import Search from './components/Search';
+import './App.css'
 
 function App() {
   
